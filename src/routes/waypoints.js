7778f12eeb8a16244831a578e55ba6d12a5c63c2
Raul <raulfmansur@hotmail.com>
2022-06-06 23:06:53 -0300

    Delete waypoint endpoint

--- src/routes/waypoints.js
+++ src/routes/waypoints.js
@@ -24,4 +24,16 @@ router.post('/waypoints', async (req, res) => {
     }
 })
 
+router.delete("/waypoints/:id", async (req, res) => {
+    try {
+        const waypoint = await Waypoint.findByIdAndDelete(req.params.id)
+        if (!waypoint)
+            return res.status(400).send({ 'error': 'Waypoint not found' })
+
+        return res.send(waypoint)
+    } catch (err) {
+        return res.status(400).send({ 'error': 'Failed to delete waypoint' })
+    }
+})
+
 module.exports = router
